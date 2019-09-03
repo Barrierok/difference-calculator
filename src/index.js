0a0beaeb56f207d4ccf7fbeabe6e4eb909675c13
Barrier <ilyazubtsov@gmail.com>
2019-09-03 08:19:20 +0300

    add reference information

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-export default () => {
+import program from 'commander';
 
+export default () => {
+  program
+    .version('0.0.1', '-V, --vers', 'output the version number')
+    .description('Compares two configuration files and shows a difference.');
+
+  program.parse(process.argv);
 };
