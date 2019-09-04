e7abe92e433ed73a7e3bea8260bac81c61b62b08
Barrier <ilyazubtsov@gmail.com>
2019-09-04 09:45:16 +0300

    add read arg

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import program from 'commander';
 export default () => {
   program
     .version('0.0.1', '-V, --vers', 'output the version number')
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<firstConfig> <secondConfig>')
+    .option('-f, --format [type]', 'Output format')
+    .action((firstConfig, secondConfig) => {
+      console.log(`${firstConfig} ${secondConfig}`);
+    });
 
   program.parse(process.argv);
 };
