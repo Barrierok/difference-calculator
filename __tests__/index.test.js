257bc27b7a6ec6fd289edb150d8b6cbbdf8928a9
Barrier <ilyazubtsov@gmail.com>
2019-09-04 21:58:50 +0300

    add compare ini

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,8 @@
 import fs from 'fs';
+import path from 'path';
 import genDiff from '../src';
+
+const ext = ['.json', '.yml', '.ini'];
 
 const pathOutput1 = `${__dirname}/__fixtures__/expectedOutputs/output1.txt`;
 const pathOutput2 = `${__dirname}/__fixtures__/expectedOutputs/output2.txt`;
@@ -8,16 +11,28 @@ const pathOutput3 = `${__dirname}/__fixtures__/expectedOutputs/output3.txt`;
 const pathAfterJson = `${__dirname}/__fixtures__/configurations/after.json`;
 const pathBeforeJson = `${__dirname}/__fixtures__/configurations/before.json`;
 const pathEmptyJson = `${__dirname}/__fixtures__/configurations/empty.json`;
+
 const pathAfterYml = `${__dirname}/__fixtures__/configurations/after.yml`;
 const pathBeforeYml = `${__dirname}/__fixtures__/configurations/before.yml`;
 const pathEmptyYml = `${__dirname}/__fixtures__/configurations/empty.yml`;
 
+const pathAfterIni = `${__dirname}/__fixtures__/configurations/after.ini`;
+const pathBeforeIni = `${__dirname}/__fixtures__/configurations/before.ini`;
+const pathEmptyIni = `${__dirname}/__fixtures__/configurations/empty.ini`;
+
 test.each`
-      pathAfter   |     pathBefore    |    pathEmpty          
-  ${pathAfterJson} | ${pathBeforeJson} | ${pathEmptyJson}
-  ${pathAfterYml}  | ${pathBeforeYml}  | ${pathEmptyYml}
-  ${pathAfterJson} | ${pathBeforeYml}  | ${pathEmptyYml}
-`('compare difference', ({ pathAfter, pathBefore, pathEmpty }) => {
+      pathAfter    |     pathBefore    |    pathEmpty     |   expectedExt    
+  ${pathAfterJson} | ${pathBeforeJson} | ${pathEmptyJson} |  ${ext[0]}
+  ${pathAfterYml}  | ${pathBeforeYml}  | ${pathEmptyYml}  |  ${ext[1]}
+  ${pathAfterIni}  | ${pathBeforeIni}  | ${pathEmptyIni}  |  ${ext[2]}
+`('compare difference $expectedExt', (
+  {
+    pathAfter,
+    pathBefore,
+    pathEmpty,
+    expectedExt,
+  },
+) => {
   const expectedData1 = fs.readFileSync(pathOutput1, 'utf8');
   expect(genDiff(pathBefore, pathAfter)).toEqual(expectedData1);
 
@@ -26,4 +41,6 @@ test.each`
 
   const expectedData3 = fs.readFileSync(pathOutput3, 'utf8');
   expect(genDiff(pathEmpty, pathAfter)).toEqual(expectedData3);
+
+  expect(path.extname(pathAfter)).toEqual(expectedExt);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,12 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
 import path from 'path';
+import ini from 'ini';
 
 const dispatcher = {
   '.json': (data) => JSON.parse(data),
   '.yml': (data) => yaml.safeLoad(data),
+  '.ini': (data) => ini.parse(data),
 };
 
 export default (pathFile) => {
