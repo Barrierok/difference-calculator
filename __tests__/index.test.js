6763702e53ed2de1efcf7260576a708c7709d84f
Barrier <ilyazubtsov@gmail.com>
2019-09-04 15:50:53 +0300

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -15,3 +15,30 @@ test('check difference', () => {
 
   expect(genDiff(`${__dirname}/__fixtures__/before.json`, `${__dirname}/__fixtures__/after.json`)).toEqual(expected);
 });
+
+describe('check difference with empty', () => {
+  test('test1', () => {
+    const expected = [
+      '{',
+      formatString('host', 'hexlet.io', '-'),
+      formatString('timeout', 50, '-'),
+      formatString('proxy', '123.234.53.22', '-'),
+      formatString('follow', false, '-'),
+      '}',
+    ].join('\n');
+
+    expect(genDiff(`${__dirname}/__fixtures__/before.json`, `${__dirname}/__fixtures__/empty.json`)).toEqual(expected);
+  });
+
+  test('test2', () => {
+    const expected = [
+      '{',
+      formatString('timeout', 20, '+'),
+      formatString('verbose', true, '+'),
+      formatString('host', 'hexlet.io', '+'),
+      '}',
+    ].join('\n');
+
+    expect(genDiff(`${__dirname}/__fixtures__/empty.json`, `${__dirname}/__fixtures__/after.json`)).toEqual(expected);
+  });
+});
